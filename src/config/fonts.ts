9a8f985ad4bda66e344c21a98801f98fcfd5d3ec
gerardoluisbv <gerardoluisbv@gmail.com>
2025-04-01 11:38:58 -0500

    Initial config, componentas and some routes

---
+++ src/config/fonts.ts
@@ -0,0 +1,11 @@
+import { Lato, Montserrat_Alternates } from 'next/font/google';
+
+export const inter = Lato({ 
+  subsets: ['latin'], 
+  weight: ['400', '700'] // Add appropriate weights here
+});
+
+export const titleFont = Montserrat_Alternates({ 
+  subsets: ['latin'],
+  weight: ['500', '700'],
+});
